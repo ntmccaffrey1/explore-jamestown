2ed754a07ca7a5402e900ba0450f9696348b4315
Nolan McCaffrey <mccaffrey.nolan@gmail.com>
2026-02-03 18:05:32 -0500

    swiper changes

--- src/components/grid/EventsSwiper.client.tsx
+++ src/components/grid/EventsSwiper.client.tsx
@@ -38,7 +38,7 @@ export default function EventsSwiperClient({
         observer
         observeParents
         breakpoints={{
-          0: { slidesPerView: 1.2 },
+          0: { slidesPerView: 1.05, spaceBetween: 12 },
           640: { slidesPerView: 2.2 },
           1024: { slidesPerView: 3 },
         }}
